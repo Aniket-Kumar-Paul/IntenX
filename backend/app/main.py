996702d8482e8edfe8ae4e343697d4ccf000cfbc
Aniket Kumar Paul <aniketkumarpaul1@gmail.com>
2025-03-24 09:06:08 +0530

    Refactoring Backend to handle only sentiment and price routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,6 @@
-from app.routers import sentiment_routes
+from app.routers import sentiment_routes, auth_routes, price_routes
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import rebalancing_routes
-from routers import user_routes, asset_routes, trade_routes, auth_routes, price_routes
 
 app = FastAPI()
 
@@ -17,11 +15,7 @@ app.add_middleware(
 )
 
 # Routes
-app.include_router(user_routes.router, prefix="/api/v1", tags=["User"])
-app.include_router(asset_routes.router, prefix="/api/v1", tags=["Assets"])
-app.include_router(trade_routes.router, prefix="/api/v1", tags=["Trades"])
 app.include_router(auth_routes.router, prefix="/api/v1", tags=["Auth"])
-app.include_router(rebalancing_routes.router, prefix="/api/v1", tags=["Rebalancing"])
 app.include_router(sentiment_routes.router, prefix="/api/v1", tags=["Sentiments"])
 app.include_router(price_routes.router, prefix="/api/v1", tags=["Prices"])
 
